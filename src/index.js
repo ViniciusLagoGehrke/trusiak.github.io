673a195ca20d370cb0fe3d99e4cdac8efe267e73
Mariusz Rogowicz <trusieq@gmail.com>
2020-11-25 17:00:41 +0100

    update view of button components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-// Test import of a JavaScript function
+// JS files
 import form from './js/form/form';
 import animation from './js/animation/animation';
 import header from './js/components/header'
 import hamburger from './js/components/hamburger'
 
-// Test import of an asset
-import webpackLogo from './images/webpack-logo.svg'
-
-// Test import of styles
+// CSS files
 import './styles/style.scss'
 
