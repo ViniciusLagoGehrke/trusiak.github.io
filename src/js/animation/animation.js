38694f5acb000adcb632d2e2657c89a871c444d7
Mariusz Rogowicz <trusieq@gmail.com>
2020-11-26 10:58:47 +0100

    little change of svg element

--- src/js/animation/animation.js
+++ src/js/animation/animation.js
@@ -1,18 +1,17 @@
 import { gsap } from "gsap";
 
 const rightFoot = document.querySelector("#rightFoot");
+const leftFoot = document.querySelector("#leftFoot");
 const rightLeg = document.querySelector("#rightLeg");
-const head = document.querySelector("#head");
-const hair = document.querySelector("#hair");
-const buttons = document.querySelectorAll("button");
 
-const leafs = [document.querySelector("#leftLeaf"),
-            document.querySelector("#rightLeaf")];
+
+const tl_LEFT_FOOT = gsap.timeline({repeat: -1, yoyo: true});
+tl_LEFT_FOOT.set(leftFoot, {rotation: 5, y: 12, x:-38});
 
 const tl_FOOT = gsap.timeline({repeat: -1, yoyo: true});
-tl_FOOT.set(rightFoot, {rotation: 10, y: -3, x:-1});
-tl_FOOT.to(rightFoot, {rotation: -5,duration: .5}); 
+tl_FOOT.set(rightFoot, {rotation: 10, y: 1, x:-2});
+tl_FOOT.to(rightFoot, {rotation: -5,y: -6,duration: .5}); 
 
 const tl_LEG= gsap.timeline({repeat: -1, yoyo: true});
-tl_LEG.to(rightLeg, {rotation: -1, y: -2, duration: .5});
+tl_LEG.to(rightLeg, {rotation: -1, y: -8, duration: .5});
 
